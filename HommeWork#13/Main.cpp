12e77fe8664af2ad7904cc41d449d4333d0b92dc
sivko <sivko@DESKTOP-TQR7KFS>
2024-04-04 01:07:01 +0500

    Добавьте файлы проекта.

---
+++ HommeWork#13/Main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int h{}, w{};
+
+	std::cout << "Введите ширину шахматного поля -> ";
+	std::cin >> w;
+	std::cout << "Введите высоту шахматного поля -> ";
+	std::cin >> h;
+	for (int i = 0; i <= h + 1; i++) {
+		for (int j = 0; j <= w + 1; j++) {
+			if (i == 0 || i == h + 1)
+				std::cout << "* ";
+			else if (j == 0 || j == w + 1)
+				std::cout << "* ";
+			else if ((j+i) % 2 == 0)
+				std::cout << "# ";
+			else
+				std::cout << "  ";
+		}
+		std::cout << "\n";
+	}
+
+	return 0;
+}
