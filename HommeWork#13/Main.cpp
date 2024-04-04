1bb3e550a46d6f165f5e17f9423b47648205b113
sivko <sivko@DESKTOP-TQR7KFS>
2024-04-04 17:53:44 +0500

    Сократил конструкцию if..else

--- HommeWork#13/Main.cpp
+++ HommeWork#13/Main.cpp
@@ -9,13 +9,11 @@ int main() {
 	std::cin >> w;
 	std::cout << "Введите высоту шахматного поля -> ";
 	std::cin >> h;
-	for (int i = 0; i <= h + 1; i++) {
-		for (int j = 0; j <= w + 1; j++) {
-			if (i == 0 || i == h + 1)
+	for (int i = 0; i < h + 2; i++) {
+		for (int j = 0; j < w + 2; j++) {
+			if (i == 0 || i == h + 1 || j == 0 || j == w + 1)
 				std::cout << "* ";
-			else if (j == 0 || j == w + 1)
-				std::cout << "* ";
-			else if ((j+i) % 2 == 0)
+			else if ((j + i) % 2 == 0)
 				std::cout << "# ";
 			else
 				std::cout << "  ";
